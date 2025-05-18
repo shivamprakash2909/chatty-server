955744e679962465eb070f5e4a20ce7228b62bbb
Shivam Prakash <shivamprakash444@gmail.com>
2025-05-18 20:35:24 +0530

    Refactor environment variable usage for CORS configuration in server setup

--- src/index.js
+++ src/index.js
@@ -13,7 +13,16 @@ dotenv.config();
 const PORT = process.env.PORT;
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.ORIGIN_PROD
+        : process.env.ORIGIN,
+    credentials: true,
+  })
+);
+
 app.use("/api", healthCheckRouter);
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
--- src/lib/socket.js
+++ src/lib/socket.js
@@ -8,7 +8,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      process.env.NODE_ENV === "production"
+        ? process.env.ORIGIN_PROD
+        : process.env.ORIGIN,
+    ],
   },
 });
 
